07475d08eecf9c851bd6640c0a692f69e9b6f7a4
chennakesavapagadala <chennakesavapagadala@gmail.com>
2025-05-14 20:22:19 +0530

    Modified .env and settings.py

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
@@ -98,7 +98,7 @@ DATABASES ={
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("POSTGRES_HOST"),
-        'PORT':os.getenv("POSYGRES_PORT"),
+        'PORT':os.getenv("POSTGRES_PORT"),
 
     }
 }
